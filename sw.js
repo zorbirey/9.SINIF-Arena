18aae8b73db082470a301370ad0259b8cdb73122
zorbirey <79220778+zorbirey@users.noreply.github.com>
2026-08-20 21:57:39 +0300

    Refresh mobile PWA cache for Zeus UI update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='arena9-mobile-v1';
+const CACHE='arena9-mobile-v2';
 const ASSETS=['./mobile.html','./style.css','./mobile.css','./app.js','./pwa.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
